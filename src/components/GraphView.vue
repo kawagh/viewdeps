c4358f5dda94ca488670e787b4994214edd15451
kawagh <kawagh.dev@gmail.com>
2024-09-30 00:02:31 +0900

    focus node by name; name operations

--- src/components/GraphView.vue
+++ src/components/GraphView.vue
@@ -4,6 +4,7 @@ import * as vNG from 'v-network-graph'
 import { ref } from 'vue'
 import { useGraphStore } from '@/stores/graph'
 
+const inputValue = ref('')
 const graphStore = useGraphStore()
 const graphInstance = ref<vNG.VNetworkGraphInstance>()
 const onSelectedFile = (it: any) => {
@@ -23,21 +24,45 @@ function updateLayout(direction: 'TB' | 'LR') {
     graphStore.layout(direction)
   })
 }
+
+const focusNode = (nodeId: string) => {
+  const node = graphStore.layouts.nodes[nodeId]
+  graphInstance.value?.setViewBox({
+    left: node.x - 50,
+    top: node.y - 50,
+    right: node.x + 50,
+    bottom: node.y + 50
+  })
+}
 </script>
 
 <template>
   <div>
-    <!-- layout buttons -->
     <div>
-      <button @click="updateLayout('LR')">layout graph Left to Right</button>
-      <button @click="updateLayout('TB')">layout graph Top to Bottom</button>
+      <h1>viewdeps</h1>
       <div>
+        <h2>import graph</h2>
+        <input type="file" @change="onSelectedFile" />
+      </div>
+      <div style="margin-top: 20px">
+        <h2>layout graph</h2>
+        <button @click="updateLayout('LR')">Left to Right</button>
+        <button @click="updateLayout('TB')">Top to Bottom</button>
+      </div>
+      <div style="margin-top: 20px">
+        <h2>modify viewBox</h2>
+        <h3>entire graph</h3>
         <button @click="graphInstance?.fitToContents">Fit contents</button>
         <button @click="graphInstance?.panToCenter">To center</button>
+
+        <h3>focus to specific node</h3>
+        <input v-model="inputValue" type="text" placeholder="enter node name" />
+        <button @click="focusNode(inputValue)">focus</button>
       </div>
     </div>
-    <input type="file" @change="onSelectedFile" />
+
     <v-network-graph
+      style="margin-top: 20px"
       ref="graphInstance"
       class="graph"
       :nodes="graphStore.nodes"

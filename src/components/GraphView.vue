1ec703dfeb9e72593a967d384f4ae6307e760423
kawagh <kawagh.dev@gmail.com>
2024-09-29 20:21:10 +0900

    typed v-network-graph variables

--- src/components/GraphView.vue
+++ src/components/GraphView.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import { VNetworkGraph } from "v-network-graph"
+import * as vNG from 'v-network-graph'
 
-const nodes = {
+const nodes: vNG.Nodes = {
     node1: { name: "Node 1" },
     node2: { name: "Node 2" },
     node3: { name: "Node 3" },
     node4: { name: "Node 4" },
 }
 
-const edges = {
+const edges: vNG.Edges = {
     edge1: { source: "node1", target: "node2" },
     edge2: { source: "node2", target: "node3" },
     edge3: { source: "node3", target: "node4" },
 }
-const layouts = {
+const layouts: vNG.Layouts = {
     nodes: {
         node1: { x: 0, y: 0 },
         node2: { x: 50, y: 50 },
